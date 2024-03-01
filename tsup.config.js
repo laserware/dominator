611db0594b8dcf36d9db6a0166d9e22866a3b8cd
Mike Rourke <mike.w.rourke@gmail.com>
2024-02-29 19:48:09 -0800

    build: use tsup for build and switch to npm

---
+++ tsup.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  outExtension: () => ({ js: ".mjs" }),
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  splitting: true,
+  minify: false,
+  tsconfig: "./tsconfig.build.json",
+});
